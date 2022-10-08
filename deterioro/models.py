8305ec8c4fd0d66d041534c744bf5d4cae3d6c97
JeanPaulRF <64498696+JeanPaulRF@users.noreply.github.com>
2022-10-07 22:23:29 -0600

    Modelos

--- deterioro/models.py
+++ deterioro/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class AgenteDeterioro(models.Model):
+
+    class TAgenteDeterioro(models.TextChoices):
+        NATURAL = 'NATURAL', 'NATURAL'
+        CIRCUNSTANCIAL = 'CIRCUNSTANCIAL', 'CIRCUNSTANCIAL'
+
+    tDeterioro = models.CharField(max_length=2 , choices=TAgenteDeterioro.choices)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
