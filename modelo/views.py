5055a69b18f01be80315c484518184552d71b254
JeanPaulRF <64498696+JeanPaulRF@users.noreply.github.com>
2022-11-16 22:00:37 -0600

    .

--- modelo/views.py
+++ modelo/views.py
@@ -675,7 +675,7 @@ def deleteInspeccion(request, id_):
 def listInspeccionesUser(request, id_):
     usuario = get_object_or_404(Usuario, id=id_)
     encargado = get_object_or_404(Encargado, email=usuario.email)
-    user_inspecciones = Inspeccion.objects.filter(encargado=encargado.id, completada=False)
+    user_inspecciones = Inspeccion.objects.filter(encargado=encargado.id)
     return render(request, 'trabajosAsignadosOperario.html', { 'inspecciones' : user_inspecciones })
 
 
